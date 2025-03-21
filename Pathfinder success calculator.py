20f76e0b644f03c6709b95933bcef20c6e6b59ac
Dunak100 <hajekdaniel100@gmail.com>
2025-03-21 15:09:36 +0100

    Fixing

--- Pathfinder success calculator.py
+++ Pathfinder success calculator.py
@@ -1,19 +1,17 @@
-import random
+ac = float(input("DC: ")) 
+hit = float(input("Skil bonus: ")) 
 
-ac = float(input("AC: ")) 
-hit = float(input("Hit Bonus: ")) 
+hitChance = max((21 + hit - ac)/20, 0)
+missChance = max(1 - hitChance, 0)
 
-hitChance =(21 + hit - ac)/20
-missChance = 1 - hitChance
+critHitChance = max(hitChance - 0.5, 0)
+critMissChance = max(missChance - 0.5, 0)
 
-critHitChance = hitChance - 0.5
-critMissChance = missChance - 0.5
-
-normalHitChance = hitChance - critMissChance
-normalMissChance = missChance - critHitChance
+normalHitChance = max(hitChance - critHitChance, 0)
+normalMissChance = max(missChance - critMissChance, 0)
 
 chances = [hitChance, missChance, critHitChance, normalHitChance, normalMissChance, critMissChance]
-chances = [str(round(x*100, 2)) for x in chances]
+chances = [str(round(min(max(x, 0), 1)*100, 2)) for x in chances]
 
 print("Total chance to succeed: " + chances[0] + "%")
 print("Total chance to fail: " + chances[1] + "%")
@@ -22,3 +20,4 @@ print("Chance to crit succeed: " + chances[2] + "%")
 print("Chance to normal succeed: " + chances[3] + "%")
 print("Chance to fail: " + chances[4] + "%")
 print("Chance to crit fail: " + chances[5] + "%")
+
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Pathfinder-2e-Skill-Check-Probability-Calculator
 This simple calculator doesn't account for 1s and 20s lowering/increasing the degree of success
